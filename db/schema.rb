03f58200d612105e760a1e6a16293ea60c8c2804
kayla <kkingston09@gmail.com>
2020-08-13 16:53:23 +0100

    finished route model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_174256) do
+ActiveRecord::Schema.define(version: 2020_08_12_174821) do
 
   create_table "routes", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_174256) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
 end
